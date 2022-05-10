d16b51ece27e35b88260369bc0e521adb2e81801
SONU842001 <54150646+SONU842001@users.noreply.github.com>
2022-05-10 20:47:10 +0530

    Created layout and page

---
+++ resources/views/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+@section('main-content')
+<h4 class="pb-3">New Task</h4>
+
+<div class="card">
+    <div class="card-header">
+        First Task
+    </div>
+    <div class="card-body">
+        <div class="card-text">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, itaque quasi. Minima consequatur commodi, excepturi sed dolore aliquam quibusdam illum veniam enim quisquam, repudiandae ut cum quo nam nostrum dolores.
+        </div>
+    </div>
+</div>
+@endsection
